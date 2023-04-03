c64ea0b2c9364178987bdecd7d21ac7f8fe5d515
Sam Lyddon <lyddon.samuel@adlittle.com>
2023-04-03 15:01:45 +0100

    Revert "Add monaco editor component"
    This reverts commit a2c5fb571ddd5d245f840b3532745b7fac10d808.

--- viz-lib/src/components/visualizations/editor/index.ts
+++ viz-lib/src/components/visualizations/editor/index.ts
@@ -12,19 +12,8 @@ import Section from "./Section";
 import Switch from "./Switch";
 import TextArea from "./TextArea";
 import ContextHelp from "./ContextHelp";
-import CodeEditor from "./CodeEditor";
 
-export {
-  Section,
-  ControlLabel,
-  Checkbox,
-  Switch,
-  TextArea,
-  ContextHelp,
-  withControlLabel,
-  createTabbedEditor,
-  CodeEditor,
-};
+export { Section, ControlLabel, Checkbox, Switch, TextArea, ContextHelp, withControlLabel, createTabbedEditor };
 export const Select = withControlLabel(AntSelect);
 export const Input = withControlLabel(AntInput);
 export const InputNumber = withControlLabel(AntInputNumber);
